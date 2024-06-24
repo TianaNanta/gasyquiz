2d63f0763f6388f9c306a4fcc867e63f5b05b2c1
andrianjakananta <andrianjakananta@gmail.com>
2024-06-24 09:19:56 +0000

    Add all models to the package

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,11 @@
 from sqlmodel import SQLModel  # noqa: F401
 
 from .base import TimeStampedModel
+from .category import Category, CategoryBase, CategoryCreate, CategoryPublic, CategoryUpdate
 from .item import Item, ItemBase, ItemCreate, ItemPublic, ItemUpdate
+from .question import Question, QuestionBase, QuestionCreate, QuestionPublic, QuestionUpdate
+from .response import Response, ResponseBase, ResponseCreate, ResponsePublic, ResponseUpdate
+from .result import Result, ResultBase, ResultCreate, ResultPublic, ResultUpdate
 from .user import (
     Message,
     NewPassword,
